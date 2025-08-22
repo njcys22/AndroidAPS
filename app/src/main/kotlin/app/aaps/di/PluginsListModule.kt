e46f587fcba307fd15cfc20e3f32a62b120ccb19
njcys22 <chenyongsheng22@126.com>
2025-08-22 19:52:46 +0800

    Update PluginsListModule.kt

--- app/src/main/kotlin/app/aaps/di/PluginsListModule.kt
+++ app/src/main/kotlin/app/aaps/di/PluginsListModule.kt
@@ -304,7 +304,7 @@ abstract class PluginsListModule {
     @Binds
     @APS
     @IntoMap
-    @IntKey(//)
+    //@IntKey(310)
     abstract fun bindObjectivesPlugin(plugin: ObjectivesPlugin): PluginBase
 
     @Binds
