81c471321675ee662e56ce86ed076c0d2bad1b22
njcys22 <chenyongsheng22@126.com>
2026-01-12 06:09:00 +0800

    Update PluginsListModule.kt

--- app/src/main/kotlin/app/aaps/di/PluginsListModule.kt
+++ app/src/main/kotlin/app/aaps/di/PluginsListModule.kt
@@ -295,17 +295,17 @@ abstract class PluginsListModule {
     @IntKey(290)
     abstract fun bindStorageConstraintPlugin(plugin: StorageConstraintPlugin): PluginBase
 
-    @Binds
-    @APS
-    @IntoMap
-    @IntKey(300)
-    abstract fun bindSignatureVerifierPlugin(plugin: SignatureVerifierPlugin): PluginBase
+    //@Binds
+    //@APS
+    //@IntoMap
+    //@IntKey(300)
+    //abstract fun bindSignatureVerifierPlugin(plugin: SignatureVerifierPlugin): PluginBase
 
-    @Binds
-    @APS
-    @IntoMap
-    @IntKey(310)
-    abstract fun bindObjectivesPlugin(plugin: ObjectivesPlugin): PluginBase
+    //@Binds
+    //@APS
+    //@IntoMap
+    //@IntKey(310)
+    //abstract fun bindObjectivesPlugin(plugin: ObjectivesPlugin): PluginBase
 
     @Binds
     @AllConfigs
